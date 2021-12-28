7b432bb4df0e4775b4a52048685f5dd6f1f3d086
Yury Brigadirenko <ybrigo@gmail.com>
2021-12-28 19:29:34 +0300

    sync with upstream 1.7.3 (#1)

---
+++ src/test/java/ca/ibodrov/concord/maven/http/TestUtils.java
@@ -0,0 +1,45 @@
+package ca.ibodrov.concord.maven.http;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.eclipse.aether.DefaultRepositorySystemSession;
+
+/**
+ * Utility methods to help unit testing.
+ */
+public class TestUtils
+{
+
+    private TestUtils()
+    {
+        // hide constructor
+    }
+
+    /**
+     * Creates a new repository session whose local repository manager is initialized with an instance of
+     * {@link TestLocalRepositoryManager}.
+     */
+    public static DefaultRepositorySystemSession newSession()
+    {
+        DefaultRepositorySystemSession session = new DefaultRepositorySystemSession();
+        session.setLocalRepositoryManager( new TestLocalRepositoryManager() );
+        return session;
+    }
+}
